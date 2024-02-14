21fdebe7c9c3a73f7a7fea20bc77209d1771e59b
nickeybamby <nicholasobazei@gmail.com>
2024-02-14 23:28:19 +0100

    new nav menus design

---
+++ nav.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/nav.css">
+    
+    <title>Document</title>
+</head>
+<body>
+    <nav class="nav-menus">
+        <div class="logo"><img src="images/logo1.png" alt="a website logo"></div>
+            <ul class="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="">Vital signs</a></li>
+                <li><a href="">Mental health assessment</a></li>
+            </ul>
+        <div class="login-cont">
+            <div class="login-btn"><a href="#">Login</a></div>
+            <div class="signup-btn"><a href="#">Sign Up</a></div>
+        </div>
+    </nav>
+</body>
+</html>

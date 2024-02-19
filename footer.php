9eee695e1c5c6d682b3a5a745daddef514d90dad
nickeybamby <nicholasobazei@gmail.com>
2024-02-19 16:32:23 +0100

    made adjustments to my codes and created new files

---
+++ footer.php
@@ -0,0 +1,14 @@
+
+<body>
+    <section class="fFooter" style="background-color: #000f3a; ">
+        <div class="fContainer">
+            <p  style="color: wheat; font-size: 16px; font-family: Arial, sans-serif; line-height: 1.5;" >Home</p>
+            <p  style="color: wheat; font-size: 16px; font-family: Arial, sans-serif; line-height: 1.5;" >About Us</p>
+            <p  style="color: wheat; font-size: 16px; font-family: Arial, sans-serif; line-height: 1.5;" >Contact Us</p>
+           
+        </div>
+        <p class= "pFooter">&copy; 2024 My Website. All rights reserved.</p>
+    </section>
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,5 +1,29 @@
-<body>
-    <section class="hero">
-        
+<?php
+require_once "nav.php";
+ ?>
+ 
+ <body>
+    <section class="iHero">
+        <div class="iBlur">
+            <div class="iBg-text">VitalGuard Clinic</div>
+            <div class="iBg-text2">Securing Your Health, One Vital at a Time</div>
+        </div>
     </section>
+    <section class="iArt-bg">
+        <article class="iArt">
+            <h3>Who We Serve</h3><br>
+            <p>VitalGuard clinic is dedicated to providing comprehensive care for individuals of all ages and health conditions, with services ranging from routine check-ups to specialized treatments, we are here to support the diverse needs of our community. Our mission is to ensure every patient receives personalized attention and expert care tailored to their unique health journey.</p>
+        </article>
+        <article class="iArt">
+        <h3>What You Get</h3><br>
+            <p>From preventative screenings to advanced diagnostic testing, we offer thorough assessments to ensure early detection and proactive management of health issues. Additionally, our team provides personalized treatment plans, education, and ongoing support to empower you on your journey to optimal health and wellness. With us, you'll receive the care and attention you deserve to live your best life.</p>
+        </article>
+    </section>
+    <div class="iContact">
+        <h3>Contact Us Now and keep your health on track with VitalGuard</h3>
+        <div class="iCon">+1234567890</div>
+    </div>
 </body>
+<?php
+require_once "footer.php";
+ ?>

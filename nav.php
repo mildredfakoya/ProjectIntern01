ec81f55b59e268c92c44eecd6ce6a796b5d70550
nickeybamby <nicholasobazei@gmail.com>
2024-02-27 17:52:50 +0100

    added a contact us form page  with js validations

--- nav.php
+++ nav.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/eval_assessment.css">
     <link rel="stylesheet" href="css/css/appointment_vitals.css">
+    <link rel="stylesheet" href="css/contact.css">
     
     <title>VitalGuard Clinic</title>
 </head>
@@ -29,79 +30,67 @@
                     <a href="#" class="nav-link">About</a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">Contact</a>
+                    <a href="contact.php" class="nav-link">Contact</a>
                 </li>
                 <li class="nav-item">
                 <button class="nav-link nlogin" onclick="document.getElementById('lg_id01').style.display='block' " style="width:auto;">Login</button>
                    <!-- <a href="#" class="nav-link nlogin">Login</a>-->
                 </li>
-            
             </ul>
             <div class="hamburger">
                 <span class="bar"></span>
                 <span class="bar"></span>
                 <span class="bar"></span>
             </div>
+            <div id="lg_id01" class="lg_modal">
+              <form class="lg_modal-content lg_animate" action="/action_page.php" method="post">
+              <div class="lg_imgcontainer">
+                <span onclick="document.getElementById('lg_id01').style.display='none'" class="lg_close" title="Close Modal">&times;</span>
+                <img src="images/AVATER.jpg" alt="Avatar" class="lg_avatar">
+              </div>
+            <div class="lg_container">
+              <label for="mail"><b>Email</b></label>
+              <input type="text" placeholder="Enter Your Email" name="mail" required>
+
+              <label for="pasw"><b>Password</b></label>
+              <input type="password" placeholder="Enter Password" name="pasw" required>
+              <br>
+
+              <a href="index2.php"><button class="nav-link nlogin" type="submit">Login</button></a>
+
+              <br>
+              <br>
+
+              <label>
+                <input type="checkbox" checked="checked" name="remember"> Remember me
+              </label>
+            </div>
+            <div class="lg_container" style="">
+          <span class="lg_pasw">Forgot <a href="#" class="lg_clkble"> password?</a></span>
+            </div>
+
+            <div class="lg_container" style="">
+            
+            <span class="lg_create_acc lg_pasw">Don't have an account? <a href="#" class="lg_clkble">Register here</a></span>
+            </div>
 
 
-
-<div id="lg_id01" class="lg_modal">
-
-<form class="lg_modal-content lg_animate" action="/action_page.php" method="post">
-  <div class="lg_imgcontainer">
-    <span onclick="document.getElementById('lg_id01').style.display='none'" class="lg_close" title="Close Modal">&times;</span>
-    <img src="images/AVATER.jpg" alt="Avatar" class="lg_avatar">
-  </div>
-
-  <div class="lg_container">
-    <label for="mail"><b>Email</b></label>
-    <input type="text" placeholder="Enter Your Email" name="mail" required>
-
-    <label for="pasw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="pasw" required>
-    <br>
-
-    <button class="nav-link nlogin" type="submit">Login</button>
-
-    <br>
-    <br>
-
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-  </div>
-
-
-  <div class="lg_container" style="">
-  
-<span class="lg_pasw">Forgot <a href="#" class="lg_clkble"> password?</a></span>
-  </div>
-
-  <div class="lg_container" style="">
-  
-  <span class="lg_create_acc lg_pasw">Don't have an account? <a href="#" class="lg_clkble">Register here</a></span>
-  </div>
-
-
-</form>
-</div>
-
+          </form>
+        </div>
         </nav>
     </header>
+  <script src="scripts/nav.js"></script>
 
-
-    <script src="scripts/nav.js"></script>
-
-    <script>
+  <script>
         // Get the modal
-      var lg_modal = document.getElementById('lg_id01');
+       var lg_modal = document.getElementById('lg_id01');
 
        // When the user clicks anywhere outside of the modal, close it
-      window.onclick = function(event) {
-      if (event.target == lg_modal) {
-        lg_modal.style.display = "none";
+         window.onclick = function(event) {
+            if (event.target == lg_modal) {
+              lg_modal.style.display = "none";
+        }
     }
-}
 </script>
 
 </body>

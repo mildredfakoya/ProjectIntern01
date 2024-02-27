8e4c98c42bcade8f8a2f3beb18e1eb028698b40f
Emmanuel Adi <adiemmanuel10@gmail.com>
2024-02-27 16:07:34 +0000

    newly updated commits

--- landing.php
+++ landing.php
@@ -7,7 +7,7 @@
 <h2>Welcome to VitalGuard Clinic</h2>
 <p>Delivering inclusive healthcare to people across all age groups and health status is our hallmark. From regular examination to specific therapies, we aim to cater for the varied requirements of our clients.</p>
 
-Your Message:
+<strong>Your Message:</strong>
 <textarea name="message" rows="5" cols="5"></textarea>
 
 <p><a href="appoint_register.php" class="button-link">Book Appointment</a></p>

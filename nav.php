2d48d1c55c7737cb1f8e65db8b4db69b32e2ff04
unknown <karonstical@gmail.com>
2024-02-27 23:10:14 -0800

    for the assessment forms

--- nav.php
+++ nav.php
@@ -1,5 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+require_once "nav.php";
+ ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -31,7 +32,8 @@
                     <a href="#" class="nav-link">Contact</a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link nlogin">Login</a>
+                <button class="nav-link nlogin" onclick="document.getElementById('lg_id01').style.display='block' " style="width:auto;">Login</button>
+                   <!-- <a href="#" class="nav-link nlogin">Login</a>-->
                 </li>
             
             </ul>
@@ -41,8 +43,64 @@
                 <span class="bar"></span>
             </div>
 
+
+
+<div id="lg_id01" class="lg_modal">
+
+<form class="lg_modal-content lg_animate" action="/action_page.php" method="post">
+  <div class="lg_imgcontainer">
+    <span onclick="document.getElementById('lg_id01').style.display='none'" class="lg_close" title="Close Modal">&times;</span>
+    <img src="images/AVATER.jpg" alt="Avatar" class="lg_avatar">
+  </div>
+
+  <div class="lg_container">
+    <label for="mail"><b>Email</b></label>
+    <input type="text" placeholder="Enter Your Email" name="mail" required>
+
+    <label for="pasw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="pasw" required>
+    <br>
+
+    <button class="nav-link nlogin" type="submit">Login</button>
+
+    <br>
+    <br>
+
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+
+  <div class="lg_container" style="background-color:#f1f1f1">
+  
+<span class="lg_pasw">Forgot <a href="#"> password?</a></span>
+  </div>
+
+  <div class="lg_container" style="background-color:#f1f1f1">
+  
+  <span class="lg_create_acc">Don't have an account? <a href="#">Register here</a></span>
+  </div>
+
+
+</form>
+</div>
+
         </nav>
     </header>
     <script src="scripts/nav.js"></script>
+
+    <script>
+        // Get the modal
+      var lg_modal = document.getElementById('lg_id01');
+
+       // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+      if (event.target == lg_modal) {
+        lg_modal.style.display = "none";
+    }
+}
+</script>
+
 </body>
 </html>

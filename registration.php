7800c25877db13739146a159919c3f0cb534e1d0
Sylvain <sylviemontoute@gmail.com>
2024-02-29 17:56:39 -0400

    final registration update

--- registration.php
+++ registration.php
@@ -1,11 +1,11 @@
 <?php
-require_once "nav_loggedin.php"
+require_once "nav.php"
  ?>
 <body class="r-body">
     <div class="r-container">
         <h2>Registration</h2><br/>
-        <form id="registrationForm" name="personalinfo" method="post" onsubmit="return validatepersonalinfo();" autocomplete="off">
-          
+        <form id="registrationForm" name="personalinfo" method="post" onsubmit="return validatepersonalinfo() && validatePasswords();" autocomplete="off">
+        
             <label for="firstname">First Name:</label><br/>
             <input name="firstname" onblur="return validatefn()"; type="text" class="r-input" id="firstname"/><span id="firstnameErr"></span> <br/>
 
@@ -32,93 +32,14 @@ require_once "nav_loggedin.php"
             <label for="confirmPassword">Confirm Password:</label><br/>
             <input type="password" id="confirmPassword" name="confirmPassword" class="r-input"/> <span id="confirmPasswordErr"></span><br/>
 
-            <button type="button"  class="r-button"> Register</button>
+            <button type="submit" class="r-button"> Register</button>
+
         </form>
 
         <div class="r-output-container" id="outputContainer"></div>
     </div>
-    <script src="../scripts/registration.js"></script>
-    <script>
-        try{
-    function validatepersonalinfo() {
-    let firstname = document.forms.personalinfo.firstname.value;
-    let lastname = document.forms.personalinfo.lastname.value;
-    let dob = document.forms.personalinfo.dob.value;
-    let sex = document.forms.personalinfo.sex.value;
-    let email = document.forms.personalinfo.email.value;
-    let password = document.forms.personalinfo.password.value;
-    let confirmPassword = document.forms.personalinfo.confirmPassword.value;
-
-    if (!firstname) {
-        document.getElementById('firstnameErr').innerHTML = "First name is required";
-        document.forms.personalinfo.firstname.focus();
-        return false;
-    }
-
-    if (!lastname) {
-        document.getElementById('lastnameErr').innerHTML = "Last name is required";
-        document.forms.personalinfo.lastname.focus();
-        return false;
-    }
-
-    if (!dob) {
-        document.getElementById('dobErr').innerHTML = "Date of Birth is required";
-        document.forms.personalinfo.dob.focus();
-        return false;
-    }
-    if (!lastname) {
-        document.getElementById('lastnameErr').innerHTML = "Last name is required";
-        document.forms.personalinfo.lastname.focus();
-        return false;
-    }
-    if (!sex) {
-        document.getElementById('sexErr').innerHTML = "Sex is required";
-        document.forms.personalinfo.sex.focus();
-        return false;
-    }
-    if (!email) {
-        document.getElementById('emailErr').innerHTML = "email is required";
-        document.forms.personalinfo.email.focus();
-        return false;
-    }
-    if (!password) {
-        document.getElementById('passwordErr').innerHTML = "Password is required";
-        document.forms.personalinfo.password.focus();
-        return false;
-    }
-    if (!lastname) {
-        document.getElementById('confirmPasswordErr').innerHTML = "Confirmation of Password is required";
-        document.forms.personalinfo.confirmPassword.focus();
-        return false;
-    }
-}
-
-function validatefn() {
-    let namepattern = /^[A-Za-z\s]*$/;
-    let firstname = document.forms.personalinfo.firstname.value;
-
-    if (firstname) {
-        let x = namepattern.test(firstname);
-        if (x === false) {
-            document.getElementById('firstnameErr').innerHTML = "<br/>First name can only contain letters and white spaces";
-            document.forms.personalinfo.firstname.focus();
-            return false;
-        } else {
-            document.getElementById('firstnameErr').innerHTML = "";
-            document.forms.personalinfo.firstname.style.borderColor = 'teal';
-            return true;
-        }
-    }
-    return false;
-}
-        }
-catch(err){
-window.alert(err.message)
-}
-finally{
-
-}
-</script>
+    <script src="scripts\registration.js"></script> 
+   
 <?php
 require_once "footer.php"
  ?>    
